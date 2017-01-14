582d219ea59734398cf1352a7a0acd0a13ce3cdc
sgalutira <sheena.galutira@gmail.com>
2017-01-13 19:50:41 -1000

    Node has name, value, and neighbors

--- graphGenerator.js
+++ graphGenerator.js
@@ -1,6 +1,14 @@
 const Node = function(name, value) {
+  this.name = name;
+  this.value = value;
+  this.neighbors = [];
+};
 
-}
+// let A = new Node("A", "cat");
+// let B = new Node("B", "dog");
+// let C = new Node("C", "bird");
+// console.log(A);
+
 
 
 module.exports = Node;
